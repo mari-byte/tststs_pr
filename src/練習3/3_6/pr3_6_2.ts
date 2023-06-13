6a08406c4f8060b97a75b442d4ef9b0d823c9b88
mari-byte <zhendaolongtai26@gmail.com>
2023-06-13 23:05:09 +0900

    2023/06/13

---
+++ src/練習3/3_6/pr3_6_2.ts
@@ -0,0 +1,19 @@
+const dabee = {
+    nama: 144,
+    kiryuu: {
+        meta: 100,
+        kao: "ainyan",
+        hujii: {
+            majity: 45
+        },
+        mousou: ["supin","imagaiijiki","nagainen"]
+    }
+}
+
+
+const {nama, kiryuu:{kao}, kiryuu:{hujii:{majity}}, kiryuu:{mousou}} = dabee;
+
+console.log(nama);
+console.log(kao);
+console.log(majity);
+console.log(mousou);

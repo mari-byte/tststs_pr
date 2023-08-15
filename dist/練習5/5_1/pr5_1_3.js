a38b3aba0e3c094c3b10620cec6e61b62714ba9e
mari-byte <zhendaolongtai26@gmail.com>
2023-08-15 20:23:50 +0900

    2023/08/15

---
+++ dist/練習5/5_1/pr5_1_3.js
@@ -0,0 +1,20 @@
+"use strict";
+class Users5_1_3 {
+    constructor() {
+        this.name = "";
+        this.age = 0;
+    }
+    isAd() {
+        return this.age >= 20;
+    }
+    setAges(newAge) {
+        this.age = newAge;
+    }
+}
+const uhyo5_1_3 = new Users5_1_3();
+console.log("調査1", uhyo5_1_3.age);
+console.log("調査2", uhyo5_1_3.name);
+console.log("調査3", uhyo5_1_3.isAd());
+console.log("調査4", uhyo5_1_3.setAges(90));
+console.log("調査5", uhyo5_1_3.age);
+console.log("調査6", uhyo5_1_3.isAd());

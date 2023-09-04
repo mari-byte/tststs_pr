ba2d2583ec2e5a3fbf3e3476ad7272567933c735
mari-byte <zhendaolongtai26@gmail.com>
2023-09-04 22:26:45 +0900

    2023/09/04

---
+++ dist/練習5/5_1/pr5_1_7.js
@@ -0,0 +1,11 @@
+"use strict";
+class User5_1_7 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const haru = new User5_1_7("harutyan", 27);
+console.log(haru);
+console.log(haru.name);
+// console.log(haru.age)
---
+++ src/練習5/5_1/pr5_1_7.ts
@@ -0,0 +1,13 @@
+class User5_1_7 {
+
+    constructor(public name: string, private age: number){
+
+    }
+
+}
+
+const haru = new User5_1_7("harutyan", 27)
+
+console.log(haru)
+console.log(haru.name)
+// console.log(haru.age)
